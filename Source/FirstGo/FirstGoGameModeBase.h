686ea8e5465f2c046ee81c643eaeacfc6d5384c3
cfdj43 <cd1097@york.ac.uk>
2020-07-01 23:15:27 +0100

    Initial commit

---
+++ Source/FirstGo/FirstGoGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FirstGoGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FIRSTGO_API AFirstGoGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
